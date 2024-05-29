20728510e8a0c737c0729d6b03689d45fb4c76ce
grantorinowhiskey <92628121+grantorinowhiskey@users.noreply.github.com>
2024-05-29 17:08:19 +0200

    Changed flake.nix to nixpkgs 24.05. Disabled mutter triple buffering in configuration.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,8 +2,8 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # NixOS official package source, using the nixos-24.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
