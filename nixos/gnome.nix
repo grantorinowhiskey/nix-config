209ede75e14554c4591b2cec859b0c222a9fefa5
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-10-14 12:46:52 +0200

    Adjusted triple buffer overlay

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -1,20 +1,24 @@
 { config, pkgs, lib, ... }:
 
 let
+
   nixpkgs.overlays = [
     # GNOME 46: triple-buffering-v4-46
     (final: prev: {
       gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-        mutter = gnomePrev.mutter.overrideAttrs ( old: {
-          src = pkgs.fetchgit {
-            url = "https://gitlab.gnome.org/vanvugt/mutter.git";
-			rev = "663f19bc02c1b4e3d1a67b4ad72d644f9b9d6970";
-            sha256 = "sha256-I1s4yz5JEWJY65g+dgprchwZuPGP9djgYXrMMxDQGrs=";         
+        mutter = gnomePrev.mutter.overrideAttrs (old: {
+          src = pkgs.fetchFromGitLab  {
+            domain = "gitlab.gnome.org";
+            owner = "vanvugt";
+            repo = "mutter";
+            rev = "triple-buffering-v4-46";
+            hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
           };
-        } );
+        });
       });
     })
   ];
+
 in
 {
   # Enable the X11 windowing system.
