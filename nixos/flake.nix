5344e7001c91fcb780439243cc3685f569e57e8e
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-10-18 17:24:21 +0200

    Restructured flake for multiple hosts

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,20 +1,30 @@
 {
-  description = "A simple NixOS flake";
+  description = "A simple NixOS flake with multiple hosts";
 
   inputs = {
     # NixOS official package source, using the nixos-24.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nix-t14 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        ./gnome.nix
-        ./gaming.nix
-          ];
+    nixosConfigurations = {
+      # Configuration for host nix-t14
+      nix-t14 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nix-t14/configuration.nix
+          ./modules/gnome.nix
+          ./modules/gaming.nix
+        ];
+      };
+
+      # Configuration for another host nix-server
+      nix-n3 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nix-n3/configuration.nix
+        ];
+      };
     };
-  };  
+  };
 }
