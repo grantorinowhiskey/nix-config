e63dcd500c5eac2f9d111b82855a30d43f3dddb6
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-10-19 12:53:16 +0200

    ZFS support and locales for nix-n3

--- nixos/hosts/nix-n3/configuration.nix
+++ nixos/hosts/nix-n3/configuration.nix
@@ -16,6 +16,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # ZFS
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+  boot.zfs.extraPools = [ "media" ];
+  services.zfs.autoScrub.enable = true;
+
   # zram
   zramSwap.enable = true;
 
@@ -23,7 +29,26 @@
   networking.hostName = "nix-n3";
   networking.hostId = "8425e349";
 
+  # Select internationalisation properties.
+  i18n.defaultLocale = "sv_SE.UTF-8";
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sv_SE.UTF-8";
+    LC_IDENTIFICATION = "sv_SE.UTF-8";
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_MONETARY = "sv_SE.UTF-8";
+    LC_NAME = "sv_SE.UTF-8";
+    LC_NUMERIC = "sv_SE.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_TELEPHONE = "sv_SE.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
+  };
+
+
+  # Configure console keymap
+  console.keyMap = "sv-latin1";
+
+  # SSH
   services.openssh.enable = true;
 
   environment.systemPackages = map lib.lowPrio [
