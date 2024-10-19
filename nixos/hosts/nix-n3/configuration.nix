28b251e56ae96ac4fac9d6d9da59cfc77c6d8da2
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-10-19 15:16:58 +0200

    zfs

--- nixos/hosts/nix-n3/configuration.nix
+++ nixos/hosts/nix-n3/configuration.nix
@@ -15,7 +15,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.zfs.extraPools = [ "storage" ];
 
   # ZFS
   boot.supportedFilesystems = [ "zfs" ];
