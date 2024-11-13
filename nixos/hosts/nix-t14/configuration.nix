41a474194f29bf4fdc82dd66eb71842b9d0c6711
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-13 19:41:22 +0100

    Enabled starship and addad man alias for fish

--- nixos/hosts/nix-t14/configuration.nix
+++ nixos/hosts/nix-t14/configuration.nix
@@ -285,6 +285,15 @@
     '';
   };
 
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      man = "batman";
+    };
+  };
+
+  programs.starship.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
