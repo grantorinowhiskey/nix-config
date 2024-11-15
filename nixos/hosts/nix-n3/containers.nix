6f8bcaca970e5bcec09d576317682441a6c63bf0
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-15 21:41:50 +0100

    Added radarr to nix-n3

--- nixos/hosts/nix-n3/containers.nix
+++ nixos/hosts/nix-n3/containers.nix
@@ -1,7 +1,5 @@
 { pkgs, config, ... }:
-
 {
-
   # Enable common container config files in /etc/containers
   virtualisation.podman = {
     enable = true;
@@ -15,19 +13,33 @@
   virtualisation.oci-containers.backend = "podman";
 
   virtualisation.oci-containers.containers = {
-    "sabnzbd" = {
+    sabnzbd = {
       image = "lscr.io/linuxserver/sabnzbd:latest";
       autoStart = true;
       ports = [ "8080:8080" ];
       environment = {
-        "PUID" = "1000";
-        "PGID" = "1000";
-        "TZ" = "Europe/Stockholm";
+        PUID = "1000";
+        PGID = "1000";
+        TZ = "Europe/Stockholm";
       };
       volumes = [
         "/home/jt/appdata/sabnzbd/config:/config"
         "/home/jt/appdata/sabnzbd/downloads:/downloads"
-        "/home/jt/appdata/sabnzbd/downloads:/incomplete-downloads"
+        "/home/jt/appdata/sabnzbd/incomplete-downloads:/incomplete-downloads"
+      ];
+    };
+
+    radarr = {
+      image = "lscr.io/linuxserver/radarr:latest";
+      autoStart = true;
+      ports = [ "7878:7878" ];
+      environment = {
+        PUID = "1000";
+        PGID = "1000";
+        TZ = "Europe/Stockholm";
+      };
+      volumes = [
+        "/home/jt/appdata/radarr/config:/config"
       ];
     };
   };
