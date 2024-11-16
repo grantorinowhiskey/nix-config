d6cb1c2f8121cb896ea41db868205baa03a6d49b
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-16 04:21:29 +0100

    Removed podman from nix-n3

--- nixos/hosts/nix-n3/configuration.nix
+++ nixos/hosts/nix-n3/configuration.nix
@@ -92,18 +92,6 @@
     zellij
   ];
 
-  # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
   # Workaround to get fish as the default interactive shell, and still using bash
   # as the system-shell
   programs.bash = {
