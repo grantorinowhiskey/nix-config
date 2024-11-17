1fa0142de7bdf0d3744d4c2a3bf4d60ba962a78b
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-17 16:38:19 +0100

    Added services.nix, removed containers.nix in nix-n3

---
+++ nixos/hosts/nix-n3/services.nix
@@ -0,0 +1,38 @@
+{ pkgs, ...}:
+
+{
+  services.nginx = {
+    enable = true;
+    virtualHosts."ynso.duckdns.org" = {
+      useACMEHost = "ynso.duckdns.org";
+      forceSSL = true;
+      locations."/" = {
+        return = "200 '<html><body>It works</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email "z6lbxfnhi@mozmail.com";
+    certs."ynso.duckdns.org" = {
+      domain = "ynso.duckdns.org";
+      extraDomainNames = [ "jellyfin.ynso.duckdns.org" ];
+      dnsProvider = "duckdns";
+      dnsPropagationCheck = true;
+      # here we need a sops-nix solution to bring in DUCKDNS_TOKEN
+      credentialFiles = ;
+    };
+  };
+
+
+
+
+
+
+}
+
+ 
