a084285399ef7203127cb569ca146b5352487642
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-19 15:21:47 +0100

    Testing acme + nginx reverse proxy

--- nixos/hosts/nix-n3/services.nix
+++ nixos/hosts/nix-n3/services.nix
@@ -1,34 +1,31 @@
 {
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "z6lbxfnhi@mozmail.com";
+    certs."ynso.duckdns.org" = {
+      domain = "ynso.duckdns.org";
+      extraDomainNames = [ "jellyfin.ynso.duckdns.org" ];
+      dnsProvider = "duckdns";
+      dnsPropagationCheck = true;
+      # here we need a sops-nix solution to bring in DUCKDNS_TOKEN
+      credentialFiles = "/run/secrets/duckdns-token";
+    };
+  };
+
+  users.users.nginx.extraGroups = [ "acme" ];
+  
   services.nginx = {
     enable = true;
-    virtualHosts.localhost = {
+    virtualHosts."jellyfin.ynso.duckdns.org" = {
+      useACMEHost = "ynso.duckdns.org";
+      forceSSL = true;
       locations."/" = {
         return = "200 '<html><body>It works</body></html>'";
         extraConfig = ''
           default_type text/html;
-          '';
+        '';
       };
     };
   };
-
-  # security.acme = {
-  #   acceptTerms = true;
-  #   defaults.email = "z6lbxfnhi@mozmail.com";
-  #   certs."ynso.duckdns.org" = {
-  #     domain = "ynso.duckdns.org";
-  #     extraDomainNames = [ "jellyfin.ynso.duckdns.org" ];
-  #     dnsProvider = "duckdns";
-  #     dnsPropagationCheck = true;
-  #     # here we need a sops-nix solution to bring in DUCKDNS_TOKEN
-  #     credentialFiles = "/run/secrets/duckdns-token";
-  #   };
-
-  #   users.users.nginx.extraGroups = [ "acme" ];
-  # };
-
-
-
-
-
 
 }
