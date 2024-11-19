d8f49c5b26dafc9f6f1e0a6aae2bdb9b8b1375c1
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-19 13:59:21 +0100

    Super basic nginx

--- nixos/hosts/nix-n3/services.nix
+++ nixos/hosts/nix-n3/services.nix
@@ -1,12 +1,6 @@
 {
   services.nginx = {
     enable = true;
-    virtualHosts.localhost = {
-      locations."/" = {
-        return = "200 '<html><body>It works</body></html>'";
-        extraConfig = ''default_type text/html;'';
-      };
-    };
   };
 
   # security.acme = {
