db903821392762728a77a9f1d76ef92d3da1620e
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-11-22 01:03:46 +0100

    nginx config for dynamic UI in radarr

--- nixos/hosts/nix-n3/services.nix
+++ nixos/hosts/nix-n3/services.nix
@@ -49,6 +49,11 @@
         forceSSL = true;
         locations."/" = {
           proxyPass = "http://127.0.0.1:7878";
+          extraConfig =''
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $http_connection;
+          '';
         };
       };
 
