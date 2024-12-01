d6ce2081ede3a2ab78f60467decb241e0d6b1c58
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-12-02 00:29:34 +0100

    Added home-manager to try with t14

--- nixos/hosts/nix-t14/configuration.nix
+++ nixos/hosts/nix-t14/configuration.nix
@@ -54,7 +54,7 @@
   # nixpkgs.config.packageOverrides = pkgs: {
       # intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
     # };
-    hardware.opengl = {
+    hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
@@ -134,6 +134,7 @@
    git
    gnupg1
    gocryptfs
+   home-manager
    htop
    hugo
    intel-gpu-tools
