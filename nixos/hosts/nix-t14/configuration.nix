b1907b577fb48c84f1335e83ae8e3cf05ec8e24d
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2024-12-07 16:30:51 +0100

    Added intel-compute-runtime to nix-t14

--- nixos/hosts/nix-t14/configuration.nix
+++ nixos/hosts/nix-t14/configuration.nix
@@ -57,9 +57,8 @@
     hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        # intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        # libvdpau-va-gl
+        intel-media-driver
+        intel-compute-runtime
       ];
     };
     environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
