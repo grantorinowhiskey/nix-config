853fbe3a3fae292bcb6578283bba65383d54114d
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-01-22 23:53:40 +0100

    Opened port 45876 for beszel on n3

--- nixos/hosts/nix-n3/configuration.nix
+++ nixos/hosts/nix-n3/configuration.nix
@@ -184,7 +184,7 @@
   services.tailscale.enable = true;
 
   # firwall
-  networking.firewall.allowedTCPPorts = [ 80 81 443 8080 ];
+  networking.firewall.allowedTCPPorts = [ 80 81 443 8080 45876 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
