1c95d9a1726fca03a021a13fa3fe6842f56b46b5
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-04-20 16:03:14 +0200

    Opened default ports for syncthing on n3

--- nixos/hosts/nix-n3/configuration.nix
+++ nixos/hosts/nix-n3/configuration.nix
@@ -197,6 +197,7 @@
   # Syncthing
   services.syncthing = { 
     enable = true;
+    openDefaultPorts = true;
     user = "jt";
     dataDir = "/home/jt/appdata/syncthing/data";    # Default folder for new synced folders
     configDir = "/home/jt/appdata/syncthing/config";   # Folder for Syncthing's settings and keys
