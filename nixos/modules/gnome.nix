5e388c5ba9c70e2735c509ab56da026ca6698846
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-04-25 17:36:25 +0200

    Added 'services.gnome.gnome-keyring.enable = true;' to gnome.nix

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -15,6 +15,9 @@
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
+
+  # Unlock keyring on autologin
+  services.gnome.gnome-keyring.enable = true;
   
   # Configure keymap in X11
   services.xserver = {
