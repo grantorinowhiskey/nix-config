ea188bd410a800da6e8ba7f469c5e8cd1dd0636d
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-04-25 17:48:53 +0200

    Removed autologin options for gnome.nix since keyring needs to be unlocked

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   # Enable the X11 windowing system.
@@ -7,17 +7,6 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "jt";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  # Unlock keyring on autologin
-  services.gnome.gnome-keyring.enable = true;
   
   # Configure keymap in X11
   services.xserver = {
