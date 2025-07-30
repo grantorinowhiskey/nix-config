421d08cfbe6c17e2b8a248caa30747ea1ae6f575
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-07-30 16:27:03 +0200

    Installed lutris

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -17,7 +17,7 @@
 
     environment.systemPackages = with pkgs; [
       heroic # install heroic launcher # Insecure version of electron 2025-05-21, resorting to flatpak.
-      # lutris # install lutris launcher
+      lutris # install lutris launcher
       # mumble # install voice-chat
       protonup-qt # GUI for installing custom Proton versions like GE_Proton
       # (retroarch.override {
