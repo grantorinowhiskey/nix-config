7bdf799f0654ed698e3ed45b2139fa47f41d0892
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-08-05 21:01:26 +0200

    Many configs

--- nixos/hosts/nix-vps-hetzner/configuration.nix
+++ nixos/hosts/nix-vps-hetzner/configuration.nix
@@ -19,8 +19,22 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
-  services.openssh.enable = true;
 
+  # networking
+  systemd.network.enable = true;
+  networking.hostName = "nix-vps-hetzner";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "sv_SE.UTF-8";
+  console = {
+  #   font = "Lat2-Terminus16";
+    keyMap = "sv-latin1";
+  };
+  
+  # packages
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
@@ -39,6 +53,41 @@
      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM8OBmgPxRcvVdjXjXc+XGfVY2/zE1pqYM1VHkIqVOGf jt@nixos-t14" 
     ];
   };
+
+  # SSH-settings
+  services.openssh = {
+  	enable = true;
+  	settings = {
+  		PasswordAuthentication = false;
+      PermitRootLogin = "yes";
+  	};
+  };
+
+  # sudo
+  security.sudo = {
+  	enable = true;
+  	wheelNeedsPassword = false;
+  };
+
+  # packages
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  # Workaround to get fish as the default interactive shell, and still using bash # Trying out with home-manager
+  # as the system-shell
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  programs.fish.enable = true;
 
   system.stateVersion = "25.05";
 }
