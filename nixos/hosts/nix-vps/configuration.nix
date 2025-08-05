176b784990911035f25f4337279473354854bbfa
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-08-05 20:22:04 +0200

    New nix-vps configurations

---
+++ nixos/hosts/nix-vps/configuration.nix
@@ -0,0 +1,36 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+} @ args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+
+  disko.devices.disk.main.device = "/dev/sda";
+
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  services.openssh.enable = true;
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys =
+  [
+    # change this to your ssh key
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM8OBmgPxRcvVdjXjXc+XGfVY2/zE1pqYM1VHkIqVOGf jt@nixos-t14"
+  ];
+
+  system.stateVersion = "25.05";
+}
