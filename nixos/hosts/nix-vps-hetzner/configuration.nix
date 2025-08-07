ece51981c41ff3d508f8a3d82643cd27682a013a
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-08-07 19:27:21 +0200

    adjusted key

--- nixos/hosts/nix-vps-hetzner/configuration.nix
+++ nixos/hosts/nix-vps-hetzner/configuration.nix
@@ -49,9 +49,10 @@
   users.users.jt = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    authorizedKeys.keys = [
+    openssh.authorizedKeys.keys = [
      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM8OBmgPxRcvVdjXjXc+XGfVY2/zE1pqYM1VHkIqVOGf jt@nixos-t14" 
     ];
+    
   };
 
   # SSH-settings
