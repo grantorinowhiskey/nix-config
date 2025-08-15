a138648c7c7870e2cb9591bd0780a7899f84b847
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-08-15 20:39:25 +0200

    Packages for niri

--- nixos/modules/niri.nix
+++ nixos/modules/niri.nix
@@ -3,6 +3,16 @@
 {
   programs.niri.enable = true;
 
+  # Other necessities
+  security.polkit.enable = true; # polkit
+  services.gnome.gnome-keyring.enable = true; # secret service
+  security.pam.services.swaylock = {};
+
+  programs.waybar.enable = true; # top bar
+
+
+  environment.systemPackages = with pkgs; [ fuzzel swaylock mako swayidle ];
+
 
   
 }
