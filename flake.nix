4ea164a1cbc4fa94d43c88b6b7c03c8600f43511
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-12-02 09:10:22 +0100

    Changed nixpkgs input to 25.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "A simple NixOS flake with multiple hosts";
 
   inputs = {
-    # NixOS official package source, using the nixos-24.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
     disko.url = "github:nix-community/disko";
