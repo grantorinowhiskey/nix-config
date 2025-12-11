077b0ad89adaf17b4f1a723f141d3b87613239f4
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-12-11 14:28:57 +0100

    Renamed option for displayManager and desktopManager

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -5,8 +5,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -16,27 +16,30 @@
 
   # Install GNOME Extensions and dark theme for GTK3
   environment.systemPackages = with pkgs; [
-   adw-gtk3
-   gnomeExtensions.alphabetical-app-grid
-   gnomeExtensions.blur-my-shell
-   gnomeExtensions.caffeine
-   gnomeExtensions.middle-click-to-close-in-overview
-   gnomeExtensions.mullvad-indicator
-   gnomeExtensions.battery-health-charging
-   gnomeExtensions.tailscale-qs
-   gnome-tweaks
+    adw-gtk3
+    gnomeExtensions.alphabetical-app-grid
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.caffeine
+    gnomeExtensions.middle-click-to-close-in-overview
+    gnomeExtensions.mullvad-indicator
+    gnomeExtensions.battery-health-charging
+    gnomeExtensions.tailscale-qs
+    gnome-tweaks
   ];
 
   # Exclusion of certain gnome applications
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-    gnome-console
-    gnome-music
-    epiphany # web browser
-    geary # email reader
-    gnome-characters
-    totem # video player
-    gnome-system-monitor
-  ]);
+  environment.gnome.excludePackages = (
+    with pkgs;
+    [
+      gnome-photos
+      gnome-tour
+      gnome-console
+      gnome-music
+      epiphany # web browser
+      geary # email reader
+      gnome-characters
+      totem # video player
+      gnome-system-monitor
+    ]
+  );
 }
