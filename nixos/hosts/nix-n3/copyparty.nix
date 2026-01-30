6c0146ac20124516d7d756ed73ad9530e9f886a5
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2026-01-30 22:14:12 +0100

    configured copyparty

--- nixos/hosts/nix-n3/copyparty.nix
+++ nixos/hosts/nix-n3/copyparty.nix
@@ -3,5 +3,30 @@
 {
   services.copyparty = {
     enable = true;
+    user = "copyparty";
+    group = "copyparty";
+
+    settings = {
+      i = "0.0.0.0";
+      p = [
+        3210
+        3211
+      ];
+    };
+
+    accounts = {
+      jt = {
+        passwordFile = "/run/keys/copyparty/jt_password";
+      };
+    };
+
+    volumes = {
+      "/" = {
+        path = "/mnt/copyparty";
+        r = "*";
+        rw = [ "jt" ];
+      };
+    };
+    openFilesLimit = 8192;
   };
 }
