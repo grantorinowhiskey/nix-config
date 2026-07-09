64a486a24b974680700138707a3d0157389d05b0
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2026-07-09 14:13:26 +0200

    Reformatting and cleanup

--- flake.nix
+++ flake.nix
@@ -3,92 +3,74 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # using nixos-unstable
-    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      # nixpkgs-unstable,
-      sops-nix,
-      disko,
-      ...
-    }@inputs:
-    {
-      nixosConfigurations = {
-        # Configuration for host nix-t14
-        nix-t14 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          # specialArgs = {
-          #   nixpkgs-unstable = import nixpkgs-unstable {
-          #     system = "x86_64-linux";
-          #     config.allowUnfree = true;
-          #   };
+  outputs = {
+    nixpkgs,
+    sops-nix,
+    disko,
+    ...
+  } @ inputs: {
+    nixosConfigurations = {
+      nix-t14 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
-          modules = [
-            ./nixos/hosts/nix-t14/configuration.nix
-            ./nixos/modules/gnome.nix
-            ./nixos/modules/gaming.nix
-            ./nixos/modules/niri.nix
-            sops-nix.nixosModules.sops
-            {
-              sops = {
-                defaultSopsFile = ./secrets/secrets.yaml;
-                age.keyFile = "/home/jt/.config/sops/age/keys.txt";
-                secrets = {
-                  dokument-crypt = {
-                    owner = "jt";
-                  };
+        modules = [
+          ./nixos/hosts/nix-t14/configuration.nix
+          ./nixos/modules/gnome.nix
+          ./nixos/modules/gaming.nix
+          ./nixos/modules/niri.nix
+          sops-nix.nixosModules.sops
+          {
+            sops = {
+              defaultSopsFile = ./secrets/secrets.yaml;
+              age.keyFile = "/home/jt/.config/sops/age/keys.txt";
+              secrets = {
+                dokument-crypt = {
+                  owner = "jt";
                 };
               };
-            }
-          ];
-        };
+            };
+          }
+        ];
+      };
 
-        # Configuration for another host nix-server
-        nix-n3 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          # specialArgs = {
-          #   nixpkgs-unstable = import nixpkgs-unstable {
-          #     system = "x86_64-linux";
-          #     config.allowUnfree = true;
-          #   };
+      nix-n3 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
-          modules = [
-            ./nixos/hosts/nix-n3/configuration.nix
-            ./nixos/hosts/nix-n3/hardware-configuration.nix
+        modules = [
+          ./nixos/hosts/nix-n3/configuration.nix
+          ./nixos/hosts/nix-n3/hardware-configuration.nix
 
+          sops-nix.nixosModules.sops
 
-            sops-nix.nixosModules.sops
-
-            {
-
-              sops = {
-                defaultSopsFile = ./secrets/secrets.yaml;
-                age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-              };
-            }
-          ];
-        };
-        nix-vps = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            disko.nixosModules.disko
-            ./nixos/hosts/nix-vps/configuration.nix
-            sops-nix.nixosModules.sops
-          ];
-        };
-        nix-vps-hetzner = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            disko.nixosModules.disko
-            ./nixos/hosts/nix-vps/configuration.nix
-            sops-nix.nixosModules.sops
-          ];
-        };
+          {
+            sops = {
+              defaultSopsFile = ./secrets/secrets.yaml;
+              age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+            };
+          }
+        ];
+      };
+      nix-vps = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./nixos/hosts/nix-vps/configuration.nix
+          sops-nix.nixosModules.sops
+        ];
+      };
+      nix-vps-hetzner = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./nixos/hosts/nix-vps/configuration.nix
+          sops-nix.nixosModules.sops
+        ];
       };
     };
+  };
 }
